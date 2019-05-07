abec5027483e5d8e4897af2a988ef1a543e24011
Caozhaoyao <shanyouyuhuai@qq.com>
2019-05-07 11:17:16 +0800

    组员山有余淮写了TestWeb

---
+++ src/main/java/com/oracle/controller/TestWeb.java
@@ -0,0 +1,11 @@
+package com.oracle.controller;
+
+public class TestWeb {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("历历万乡");
+	}
+
+}
