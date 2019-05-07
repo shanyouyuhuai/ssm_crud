fe1350df4be5341524b45787e6c712dc7b9bd6a2
Caozhaoyao <shanyouyuhuai@qq.com>
2019-05-07 10:59:50 +0800

    提交测试类TestApp

---
+++ src/main/java/com/oracle/controller/TestApp.java
@@ -0,0 +1,11 @@
+package com.oracle.controller;
+
+public class TestApp {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("测试成功");
+	}
+
+}
